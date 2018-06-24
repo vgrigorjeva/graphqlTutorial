cf225aa1823cec50ffbf76aca8443f47d59850e2
Valerija Grigorjeva <vgrigorjeva@gmail.com>
2018-06-24 12:17:20 +0200

    add new graphqlobjecttype

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 //dummy data
 var books = [
@@ -10,14 +10,29 @@ var books = [
     { name: "Name3", genre: "comedy", id: '3' },
 ];
 
+var authors = [
+    { name: "author1", age: 23, id: '1' },
+    { name: "author2", age: 54, id: '2' },
+    { name: "author3", age: 12, id: '3' },
+];
+
 //define object type with different fields
 //wrapped inside function
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
     })
 });
 
@@ -27,13 +42,23 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString} },
+            args: { id: { type: GraphQLID} },
             //when query is received fire this function
             resolve(parent, args){
+                console.log(typeof(args.id))
                 //code to get data from db/other source
                 return _.find(books, { id: args.id });
             }
-        }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID} },
+            //when query is received fire this function
+            resolve(parent, args){
+                //code to get data from db/other source
+                return _.find(authors, { id: args.id });
+            }
+        },
     }
 });
 
